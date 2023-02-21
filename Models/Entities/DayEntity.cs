1d59ba7156c31199bccb3c57e93e73b43c98fe6c
big-pablo <fckem.nggaz@gmail.com>
2023-02-21 22:39:38 +0700

    Поднял базу данных Удалил InTimeContext, теперь контекст лежит в папке моделей, плюс мне показалось, что таблицы удачнее назвал (Спорно) Удалил из пакетов NuGet какую-то дрянь для MySQL В program.cs записал всё для БД Убрал лишние сущности и сделал все наследниками BaseEntity

---
+++ Models/Entities/DayEntity.cs
@@ -0,0 +1,9 @@
+namespace CloneIntime.Entities;
+
+public class DayEntity:BaseEntity
+{
+    public WeekEnum DayName { get; set; }
+    public DateTime Date { get; set; }
+    public GroupEntity Group { get; set; }
+    public List<TimeSlotEntity> Lessons { get; set; }
+}   
